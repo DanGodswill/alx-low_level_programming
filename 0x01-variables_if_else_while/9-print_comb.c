78fe7b34b800b761390f7f3d27bd37c473af6051
DanGodswill <09daysmor@gmail.com>
2023-02-18 03:32:33 -0800

    to print osingle number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,22 +5,22 @@
 */
 int main(void)
 {
-    int i = 0;
+int i = 0;
 
-    while (i < 100)
-    {
-        if (i / 10 < i % 10)
-        {
-            putchar(i / 10 + '0');
-            putchar(i % 10 + '0');
-            if (i < 89)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-        i++;
-    }
-    putchar('\n');
-    return (0);
+while (i < 100)
+{
+if (i / 10 < i % 10)
+{
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
+if (i < 89)
+{
+putchar(',');
+putchar(' ');
+}
+}
+i++;
+}
+putchar('\n');
+return (0);
 }
