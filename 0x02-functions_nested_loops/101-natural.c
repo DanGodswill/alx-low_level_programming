387e8b1b3f33deb2e5e41d787d86acb4af6a254e
DanGodswill <09daysmor@gmail.com>
2023-02-21 19:08:43 -0800

    correction

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,18 +6,20 @@
 *
 * Return: Always 0.
 */
-int main(void)
-{
-  int i, z = 0;
 
-  while (i < 1024)
-  {
-    if ((i % 3 == 0) || (i % 5 == 0))
+int main(void)
+
+{
+int i, z = 0;
+
+while (i < 1024)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
 {
 z += i;
-  }
+}
 i++;
 }
-  printf("%d\n", z);
-  return (0);
+printf("%d\n", z);
+return (0);
 }
