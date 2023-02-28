b0dd274d79dd977c9d468393fe75ccb5f48a4a01
DanGodswill <09daysmor@gmail.com>
2023-02-27 17:14:17 -0800

    correction

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -15,10 +15,10 @@ char c;
 srand(time(NULL));
 {
 	while (sum <= 2645)
-		c = rand() % 128;
-                sum += c;
-                putchar(c);
-        }
-        putchar(2772 - sum);
-        return (0);
+c = rand() % 128;
+	sum += c;
+	putchar(c);
+}
+putchar(2772 - sum);
+return (0);
 }
